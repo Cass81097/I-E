45ade0b82a09ead921aceeb769f70bf979d42fc3
Cass81097 <truonghoanganh.810@gmail.com>
2023-11-17 15:59:08 +0700

    menu responsive

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.hamburger-menu').on('click', function () {
+        $('.navbar-school-menu').toggle(); 
+
+        // Kiểm tra nếu menu đang hiển thị
+        if ($('.navbar-school-menu').is(':visible')) {
+            $('.navbar-school-menu').css("display", "flex");
+            $(".menu-loading").addClass('modal-backdrop');
+        } else {
+            $(".menu-loading").removeClass('modal-backdrop');
+            $('.navbar-school-menu').css("display", ""); // Đặt lại thuộc tính display
+        }
+    });
+});
--- js/search.js
+++ js/search.js
@@ -1,20 +1,32 @@
 $(document).ready(function () {
-    function toggleSearch() {
-        let searchInput = $("#searchInput");
-        searchInput.toggleClass("visible");
-
-        if (searchInput.hasClass("visible")) {
-            $(document).on('click.toggleSearchEvent', function (event) {
-                if (!searchInput.is(event.target) && searchInput.has(event.target).length === 0) {
-                    searchInput.removeClass("visible");
-                    $(document).off('click.toggleSearchEvent');
-                }
-            });
-        }
+    // Hàm để hiển thị search-input
+    function showSearchInput() {
+        let searchInput = $(".search-input");
+        searchInput.addClass("visible");
     }
 
+    // Hàm để ẩn search-input
+    function hideSearchInput() {
+        let searchInput = $(".search-input");
+        searchInput.removeClass("visible");
+    }
+
+    // Khi click vào icon-search, hiển thị search-input
     $('.icon-search').on('click', function (event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện lan truyền
+        showSearchInput();
+    });
+
+    // Khi click vào document, ẩn search-input nếu click ngoài khu vực của nó
+    $(document).on('click', function (event) {
+        let searchInput = $(".search-input");
+        if (!searchInput.is(event.target) && searchInput.has(event.target).length === 0 && searchInput.hasClass("visible")) {
+            hideSearchInput();
+        }
+    });
+
+    // Ngăn chặn việc ẩn search-input khi click vào chính nó
+    $('.search-input').on('click', function (event) {
         event.stopPropagation();
-        toggleSearch();
     });
 });
