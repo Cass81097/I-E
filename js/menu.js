eefab1ab1e725d8052b1f1928b8df10a51172b88
Cass81097 <truonghoanganh.810@gmail.com>
2023-11-17 16:59:19 +0700

    anim menu

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
     $('.hamburger-menu').on('click', function () {
+        // Toggle menu và icon tìm kiếm
         $('.navbar-school-menu').toggle(); 
         $('.icon-search').toggle(); 
 
+        // Toggle class 'visible'
+        $('.navbar-school-menu').toggleClass("visible");
+
         // Kiểm tra nếu menu đang hiển thị
-        if ($('.navbar-school-menu').is(':visible')) {
+        if ($('.navbar-school-menu').hasClass("visible")) {
             $('.navbar-school-menu').css("display", "flex");
             $(".menu-loading").addClass('modal-backdrop');
         } else {
